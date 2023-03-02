72989e5cd99ea2abe992df36564d5055c3d93aef
Vahe Abazyan <vaheabazyan@preezma.com>
2023-03-02 14:25:27 +0400

    Initial Commit

---
+++ GmailDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GmailDemo
+//
+//  Created by Vahe Abazyan on 02.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
